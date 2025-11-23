a16a77a612b1afd5be6c4dd890553b7542c807c5
Khusanbek Gafurov, Msc. <x.gafurov@gmail.com>
2025-11-23 22:29:44 +0500

    Add files via upload

---
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.todolist;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editTextTask;
+    Button buttonAdd;
+    ListView listViewTasks;
+
+    ArrayList<String> taskList;
+    ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextTask = findViewById(R.id.editTextTask);
+        buttonAdd = findViewById(R.id.buttonAdd);
+        listViewTasks = findViewById(R.id.listViewTasks);
+
+        taskList = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, taskList);
+        listViewTasks.setAdapter(adapter);
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String task = editTextTask.getText().toString();
+                if (!task.isEmpty()) {
+                    taskList.add(task);
+                    adapter.notifyDataSetChanged();
+                    editTextTask.setText(""); // clear input
+                }
+            }
+        });
+    }
+}
